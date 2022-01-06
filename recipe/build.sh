decf39c059303287fc7ff68bae1b29fd045cf602
Leo Fang <leo80042@gmail.com>
2022-01-06 18:53:23 -0500

    embed PTX for CC >= 6.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,9 @@
 
 if [[ $target_platform == linux-aarch64 ]]; then
     # it takes too much time to compile, so we reduce the supported archs on aarch64
-    export NVCC_GENCODE="-gencode=arch=compute_60,code=sm_60 \
-                         -gencode=arch=compute_70,code=sm_70 \
-                         -gencode=arch=compute_80,code=sm_80 \
-                         -gencode=arch=compute_80,code=compute_80"
+    export NVCC_GENCODE="-gencode=arch=compute_60,code=compute_60,sm_60 \
+                         -gencode=arch=compute_70,code=compute_70,sm_70 \
+                         -gencode=arch=compute_80,code=compute_80,sm_80"
     make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart" NVCC_GENCODE="$NVCC_GENCODE"
 else
     make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart"
