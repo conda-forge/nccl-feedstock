63d49d7b2ffee10218a8ac7487a97c4e773aefaf
Leo Fang <leof@nvidia.com>
2023-04-15 21:51:41 -0400

    Update build.sh
    try to use nccl's default gencode for all platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 
-if [[ $target_platform == linux-aarch64 || ($target_platform == linux-ppc64le && $cuda_compiler_version != "10.2")]]; then
-    # it takes too much time to compile, so we reduce the supported archs on aarch64
-    export NVCC_GENCODE="-gencode=arch=compute_60,code=[compute_60,sm_60] \
-                         -gencode=arch=compute_70,code=[compute_70,sm_70] \
-                         -gencode=arch=compute_80,code=[compute_80,sm_80]"
-    make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static" NVCC_GENCODE="$NVCC_GENCODE"
-else
-    make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static"
-fi
+#if [[ $target_platform == linux-aarch64 || ($target_platform == linux-ppc64le && $cuda_compiler_version != "10.2")]]; then
+#    # it takes too much time to compile, so we reduce the supported archs on aarch64
+#    export NVCC_GENCODE="-gencode=arch=compute_60,code=[compute_60,sm_60] \
+#                         -gencode=arch=compute_70,code=[compute_70,sm_70] \
+#                         -gencode=arch=compute_80,code=[compute_80,sm_80]"
+#    make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static" NVCC_GENCODE="$NVCC_GENCODE"
+#else
+#    make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static"
+#fi
 
+make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static"
 make install PREFIX="${PREFIX}"
 
 # Delete static library
