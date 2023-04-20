4701a6da180eadbaaf5f09be6c280bb1a40092e1
Peter Andreas Entschev <peter@entschev.com>
2023-04-20 08:51:53 -0700

    Set `CFLAGS`/`CXXFLAGS` with missing includes/libraries

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+[[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
+[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
+[[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
+
+export CFLAGS="${CFLAGS} -I${PREFIX}/${targetsDir}/include"
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/${targetsDir}/include"
+
 if [[ $target_platform == linux-aarch64 || ($target_platform == linux-ppc64le && $cuda_compiler_version != "10.2")]]; then
     # it takes too much time to compile, so we reduce the supported archs on aarch64
     export NVCC_GENCODE="-gencode=arch=compute_60,code=[compute_60,sm_60] \
