b8acde0d55c0e0f9cccc39827699d339274d69c1
jakirkham <jakirkham@gmail.com>
2023-05-31 16:09:32 -0700

    Call `make` once (outside `if`)
    Borrow simplifications from Peter's CUDA 12 PR to call `make` once.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 
+EXTRA_ARGS="CUDA_HOME=\"${CUDA_HOME}\" CUDARTLIB=\"cudart_static\""
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
     if [[ $target_platform == linux-aarch64 ]]; then
-        make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static" CUDA_LIB="${CUDA_HOME}/targets/sbsa-linux/lib/"
+        EXTRA_ARGS="${EXTRA_ARGS} CUDA_LIB=\"${CUDA_HOME}/targets/sbsa-linux/lib/\""
     elif [[ $target_platform == linux-ppc64le ]]; then
-        make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static" CUDA_LIB="${CUDA_HOME}/targets/ppc64le-linux/lib/"
+        EXTRA_ARGS="${EXTRA_ARGS} CUDA_LIB=\"${CUDA_HOME}/targets/ppc64le-linux/lib/\""
     else
         echo "not supported"
         exit -1
     fi
-else
-    make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static"
 fi
+
+# `eval` is needed here for proper `${...}` expansion
+eval make -j${CPU_COUNT} src.lib ${EXTRA_ARGS}
+
 make install PREFIX="${PREFIX}"
 
 # Delete static library
