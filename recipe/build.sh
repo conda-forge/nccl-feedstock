d673eefb5f469f7b593a68a1888403f61b9aa886
jakirkham <jakirkham@gmail.com>
2023-05-31 15:59:28 -0700

    Drop commented code

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 
-#if [[ $target_platform == linux-aarch64 || ($target_platform == linux-ppc64le && $cuda_compiler_version != "10.2")]]; then
-#    # it takes too much time to compile, so we reduce the supported archs on aarch64
-#    export NVCC_GENCODE="-gencode=arch=compute_60,code=[compute_60,sm_60] \
-#                         -gencode=arch=compute_70,code=[compute_70,sm_70] \
-#                         -gencode=arch=compute_80,code=[compute_80,sm_80]"
-#    make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static" NVCC_GENCODE="$NVCC_GENCODE"
-#else
-#    make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static"
-#fi
-
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
     if [[ $target_platform == linux-aarch64 ]]; then
         make -j${CPU_COUNT} src.lib CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart_static" CUDA_LIB="${CUDA_HOME}/targets/sbsa-linux/lib/"
