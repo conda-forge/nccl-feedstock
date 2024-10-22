0c57c3b95e2cf4289384a48a2d8f72d22f7cbfd2
jakirkham <jakirkham@gmail.com>
2024-10-22 14:04:23 -0700

    Fix shellcheck lint
    * Quote variables in assignment
    * Use `exit 1` (`-1` is invalid)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [[ "${cuda_compiler_version}" =~ 12.* ]]; then
-  export CUDA_HOME=${BUILD_PREFIX}
+  export CUDA_HOME="${BUILD_PREFIX}"
 elif [[ "${cuda_compiler_version}" != "None" ]]; then
-  export CUDA_HOME=${CUDA_PATH}
+  export CUDA_HOME="${CUDA_PATH}"
 fi
 
-if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
-    if [[ $target_platform == linux-aarch64 ]]; then
-        export CUDA_LIB=${CUDA_HOME}/targets/sbsa-linux/lib/
-    elif [[ $target_platform == linux-ppc64le ]]; then
-        export CUDA_LIB=${CUDA_HOME}/targets/ppc64le-linux/lib/
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+    if [[ "${target_platform}" == "linux-aarch64" ]]; then
+        export CUDA_LIB="${CUDA_HOME}/targets/sbsa-linux/lib/"
+    elif [[ "${target_platform}" == "linux-ppc64le" ]]; then
+        export CUDA_LIB="${CUDA_HOME}/targets/ppc64le-linux/lib/"
     else
         echo "not supported"
-        exit -1
+        exit 1
     fi
 fi
 
